d52eeced0c8c48bc04f8efb85c17810dc1948d23
Olga <minaeva9@gmail.com>
2024-01-09 14:43:09 -0500

    modified

--- app.js
+++ app.js
@@ -1,69 +1,43 @@
 import { menuArray } from "./data.js";
-const orderBox = document.getElementById("order");
-let totalPrice = 0;
-let orders = [];
 
-//event listener
+let order = [];
+let price = 0;
+//eventlistener
 document.addEventListener("click", function (e) {
   e.preventDefault();
   if (e.target.dataset.add) {
-    addItem(e.target.dataset.add);
+    handleClickAdd(e.target.dataset.add);
   }
 });
 
-//handle add button
-function addItem(itemId) {
-  const addItemObj = menuArray.filter(function (item) {
-    return (item.id = itemId);
+function handleClickAdd(itemId) {
+  let itemObj = menuArray.filter(function (item) {
+    return item.id === itemId;
   })[0];
-  if (!orders.includes(addItemObj)) {
-    orders.push(addItemObj);
-    renderOrderItems();
-  }
-
-  // orderBox.style.display = "flex";
-  // renderOrderItems();
+  console.log(itemObj);
 }
 
-//render order item
-function getOrderHtml() {
-  let orderHtml = "";
-  orders.forEach(function (item) {
-    orderHtml += `
-  <h4>${item.name}</h4>
-<button class="remove" >remove</button>
-<p>$${item.price}</p>
-  `;
-  });
-}
-
-//rendering Menu
-function getMenu() {
-  let menuHtml = "";
-
+//menu html
+function menuHtml() {
+  let menu = "";
   menuArray.forEach(function (item) {
-    menuHtml += `
-          <div class="item">
+    menu += `
+  <div class="item">
             <h1 class="emoji">${item.emoji}</h1>
               <div class="name">
                 <h2>${item.name}</h2>
                 <p>${item.ingredients}</p>
                  <h4>$${item.price}</h4>
                </div>
-           <button class="add" data-add=${item.id}>+</button>
+           <button class="add" data-add="${item.id}">+</button>
        </div>
   `;
   });
-
-  return menuHtml;
+  return menu;
 }
 
-function render() {
-  document.getElementById("container").innerHTML = getMenu();
+function renderMenu() {
+  document.getElementById("container").innerHTML = menuHtml();
 }
 
-function renderOrderItems() {
-  document.getElementById("addItem").innerHTML = getOrderHtml();
-}
-
-render();
+renderMenu();
--- data.js
+++ data.js
@@ -2,22 +2,25 @@ export const menuArray = [
   {
     name: "Pizza",
     ingredients: ["pepperoni", "mushrom", "mozarella"],
-    id: 0,
+    id: '0',
     price: 14,
     emoji: "🍕",
+    isPurchased: false,
   },
   {
     name: "Hamburger",
     ingredients: ["beef", "cheese", "lettuce"],
     price: 12,
     emoji: "🍔",
-    id: 1,
+    id: '1',
+    isPurchased: false,
   },
   {
     name: "Beer",
     ingredients: ["grain, hops, yeast, water"],
     price: 12,
     emoji: "🍺",
-    id: 2,
+    id: '2',
+    isPurchased: false,
   },
 ];
