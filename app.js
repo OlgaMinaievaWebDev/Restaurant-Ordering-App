f7a7be77847cfc5fe00faa6fb6ce95b30537a2b2
Olga <minaeva9@gmail.com>
2024-01-08 19:18:48 -0500

    modified

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
 import { menuArray } from "./data.js";
 
+//rendering Menu
+function getMenu(menuArray) {
+  let menuHtml = "";
 
-
-//eventListeners 
-document.addEventListener('click', function (e) {
- if (e.target.dataset.pizza) {
-  console.log('click')
- }
-})
-
-//set HTML for each item
-function getFeedHtml(menuArray) {
-  return menuArray
-    .map(function (item) {
-      const { name, ingredients, id, price, emoji } = item;
-      const listIngredients = ingredients.join(",");
-     return `
-       <div class="item">
-           <h1 class="emoji">${emoji}</h1>
+  menuArray.forEach(function (menuItem) {
+    const { name, ingredients, id, price, emoji } = menuItem;
+    menuHtml += `
+          <div class="item">
+            <h1 class="emoji">${emoji}</h1>
               <div class="name">
                 <h2>${name}</h2>
-                <p>${listIngredients}</p>
+                <p>${ingredients}</p>
                  <h4>$${price}</h4>
                </div>
-           <button id="add" class="add" data-${name}="${item.id}">+</button>
+           <button id="add" class="add">+</button>
        </div>
   `;
-    })
-    .join("");
+  });
+
+  return menuHtml;
 }
 
-//render HTML
-document.getElementById("container").innerHTML = getFeedHtml(menuArray);
+function render() {
+  document.getElementById("container").innerHTML = getMenu(menuArray);
+}
+
+render();
